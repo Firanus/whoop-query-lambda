023e56dcc0249fd6a174528b3108daa28ed80aa6
Ivan Tchernev <ivan@letsdothis.com>
2021-01-01 19:59:55 +0000

    Added eslint to the project

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:import/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'import',
+  ],
+  rules: {
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import { ScheduledHandler, Handler } from 'aws-lambda';
+import type { Handler } from 'aws-lambda';
 
+// eslint-disable-next-line import/prefer-default-export
 export const handler: Handler = async (event): Promise<any> => {
+  // eslint-disable-next-line no-console
   console.log('Hello World!');
   const response = JSON.stringify(event, null, 2);
   return response;
-}
+};
